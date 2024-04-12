f009fc6c137fd933b237cd06cff632c0f152d701
w3schools-test <ponchobt.dev@gmail.com>
2024-04-12 12:56:42 -0600

    Added main and login

---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+// TODO: Add a comment indicating how this file fits into the MVC framework 
+//TODO (is it a Model, a View, or a Controller?) and what it is responsible for handling.
+
+// this is a controller its responsible for handling the homeroute
+const router = require('express').Router();
+// TODO: Add a comment describing the purpose of the get route
+router.get('/', async (req, res) => { // controlling the main endpoint
+//TODO: Add a comment describing the purpose of the render method
+//? Use handlebars to render somthing for the client
+  res.render('login');
+});
+
+module.exports = router;
