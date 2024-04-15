e72b4ff7ee899126baef32d4df2f7483c60f9dad
lage356 <fernando.lage356@gmail.com>
2024-04-15 16:58:35 -0700

    updating html routes and user routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,7 @@
-// TODO: Add a comment indicating how this file fits into the MVC framework 
-//TODO (is it a Model, a View, or a Controller?) and what it is responsible for handling.
 
-// this is a controller its responsible for handling the homeroute
 const router = require('express').Router();
-// TODO: Add a comment describing the purpose of the get route
-router.get('/', async (req, res) => { // controlling the main endpoint
-//TODO: Add a comment describing the purpose of the render method
-//? Use handlebars to render somthing for the client
-  res.render('login');
-});
+const apiRoutes = require('./api');
+const htmlRoutes = require ('./htmlRoutes');
+
 
 module.exports = router;
