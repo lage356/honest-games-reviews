df9b2997f390eb82d2ad79fa09984607b19b7821
lage356 <fernando.lage356@gmail.com>
2024-04-15 18:48:59 -0700

    updating html routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const htmlRoutes = require ('./htmlRoutes');
+const homeRoutes = require ('./homeRoutes');
 
 router.use ('/api',apiRoutes);
-router.use ('/',htmlRoutes);
+router.use ('/', homeRoutes);
 module.exports = router;
