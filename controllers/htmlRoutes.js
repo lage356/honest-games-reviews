8974ff44d11842f500fda39f3a60a09324982d21
lage356 <fernando.lage356@gmail.com>
2024-04-15 18:04:59 -0700

    updating html routes and user routes

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -5,4 +5,18 @@ router.get('/', async (req, res) => { // controlling the main endpoint
     res.render('login');
   });
 
+  router.get("/login", (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect("/");
+      return;
+    }
+  
+    res.render("login");
+  });
+
+
+
+
   module.exports = router;
+
