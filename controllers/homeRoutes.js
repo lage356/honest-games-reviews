76360a62406ade26bdc3dffeef6a13a4f915be60
lage356 <fernando.lage356@gmail.com>
2024-04-20 12:59:45 -0700

    updating commentsRoute in order to display data

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
+const { classToInvokable } = require("sequelize/lib/utils");
 const { User, Review, Comment } = require("../models");
-
 
 router.get("/", async (req, res) => {
   // controlling the main endpoint
@@ -25,33 +25,49 @@ router.get("/", async (req, res) => {
   }
 });
 
+
 router.get("/review/:id", async (req, res) => {
   try {
-    const review = await Review.findByPk(req.params.id, {
+    const reviews = await Review.findByPk(req.params.id, {
       include: [
         {
           model: Comment,
-          attributes: ["content"],
+          attributes: ["content","createdAt"],
+          include:[
+            {
+              model:User,
+              attributes:["username"],
+            }
+          ],
         },
         {
           model: User,
           attributes: ["username"],
         },
       ],
-      });
+    });
 
+    const review = reviews.get({ plain: true });
     
-    const reviews = review.get({ plain: true });
-    console.log(reviews);
+
+    const comments =review.comments.map(comment =>({
+      content: comment.content,
+      username:comment.user.username,
+      createdAt:comment.createdAt
+    }));
+
+
+  console.log(comments)
+
     res.render("singleReview", {
-      reviews,
+      review,
+      comments,
       logged_in: req.session.logged_in,
     });
   } catch (error) {
     res.status(500).json(error);
   }
 });
-
 
 router.get("/review", (req, res) => {
   // If the user is already logged in, redirect the request to another route
