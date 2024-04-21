7db6b429e3126770237fd03971141be2dc46b2e9
w3schools-test <ponchobt.dev@gmail.com>
2024-04-20 20:06:50 -0600

    Correct

--- controllers/api/avatarRoutes.js
+++ controllers/api/avatarRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const cloudinary = require("cloudinary").v2;
 
-const { Avatar } = require('../../models');
+cloudinary.config({
+  cloud_name: "dyoigdvdh",
+  api_key: "831332363759673",
+  api_secret: "uXG0Iy3RdRMJrC2COTqqS6m9PU8",
+});
 
 
 
@@ -11,18 +16,41 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 // Ruta para subir un nuevo avatar
-router.post('/', upload.single('image'), async (req, res) => {
-  try {
-    const result = await cloudinary.uploader.upload(req.file.buffer);
-    const newAvatar = await Avatar.create({
-      name_file: req.file.originalname,
-      url_file: result.secure_url
-    });
-    res.status(201).json({ imageUrl: result.secure_url });
-  } catch (error) {
-    console.error('Error al subir la imagen:', error);
-    res.status(500).json({ error: 'Hubo un error al subir la imagen.' });
-  }
+router.post("/", upload.single("image"), (req, res) => {
+  cloudinary.uploader
+    .upload_stream({ resource_type: "image" }, (error, result) => {
+      if (error) {
+        console.error(error);
+        return res
+          .status(500)
+          .json({ error: "Hubo un error al subir la imagen a Cloudinary." });
+      }
+
+      const archivo = {
+        nombre_archivo: req.file.originalname,
+        url_archivo: result.secure_url,
+      };
+
+      // connection.query(
+      //   "INSERT INTO archivos SET ?",
+      //   archivo,
+      //   (err, results) => {
+      //     if (err) {
+      //       console.error(
+      //         "Error al guardar el archivo en la base de datos:",
+      //         err
+      //       );
+      //       return res.status(500).json({
+      //         error: "Hubo un error al guardar el archivo en la base de datos.",
+      //       });
+      //     }
+      //     console.log("Archivo guardado en la base de datos:", results);
+
+      //     res.json({ imageUrl: result.secure_url });
+      //   }
+      // );
+    })
+    .end(req.file.buffer);
 });
 
 
--- public/js/upload-photo.js
+++ public/js/upload-photo.js
@@ -1,39 +1,40 @@
-  function seleccionarImagen() {
-    // Simulamos un clic en el input de archivo cuando se hace clic en la imagen
-    document.getElementById('imageInput').click();
-  }
+function seleccionarImagen() {
+  // Simulamos un clic en el input de archivo cuando se hace clic en la imagen
+  document.getElementById('imageInput').click();
+}
 
-  function mostrarVistaPrevia() {
-    const fileInput = document.getElementById('imageInput').files[0];
-    if (fileInput) {
-      const reader = new FileReader();
-      reader.onload = function(e) {
-        document.getElementById('uploadedImage').src = e.target.result;
-        document.getElementById('uploadedImage').style.display = 'block';
-      }
-      reader.readAsDataURL(fileInput);
+function mostrarVistaPrevia() {
+  const fileInput = document.getElementById('imageInput').files[0];
+  if (fileInput) {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      document.getElementById('uploadedImage').src = e.target.result;
+      document.getElementById('uploadedImage').style.display = 'block';
     }
+    reader.readAsDataURL(fileInput);
   }
+}
 
-  function subirImagen() {
-    const fileInput = document.getElementById('imageInput').files[0];
-    if (fileInput) {
-      const formData = new FormData();
-      formData.append('image', fileInput);
-      fetch('/api/avatars', {  // Asegúrate de agregar '/' al principio para indicar la ruta raíz
-        method: 'POST',
-        body: formData,
-      })
-      .then(response => response.json())
-      .then(data => {
-        document.getElementById('uploadedImage').src = data.imageUrl;
-        document.getElementById('uploadedImage').style.display = 'block';
-        var successModal = new bootstrap.Modal(document.getElementById('successModal'), {keyboard: false});
-        successModal.show();
-      })
-      .catch(error => {
-        console.error('Error al subir la imagen:', error);
-      });
-    }
+function subirImagen() {
+  const fileInput = document.getElementById('imageInput').files[0];
+  if (fileInput) {
+    const formData = new FormData();
+    formData.append('image', fileInput);
+
+    fetch('/api/avatars', {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('uploadedImage').src = data.imageUrl;
+      document.getElementById('uploadedImage').style.display = 'block';
+      // Mostrar el modal de éxito después de cargar la imagen
+      var successModal = new bootstrap.Modal(document.getElementById('successModal'), {keyboard: false});
+      successModal.show();
+    })
+    .catch(error => {
+      console.error('Error al subir la imagen:', error);
+    });
   }
-  
+}
